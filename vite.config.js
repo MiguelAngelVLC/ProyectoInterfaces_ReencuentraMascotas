d0510bb9abbfe567d186cf1d9920953da2259bd8
Miguel Angel M. Z. <Miguel_m.z@hotmail.com>
2026-02-04 01:45:50 +0100

    Codigo como antes

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
 export default defineConfig({
-  build: {
+    base: '/ProyectoInterfaces_ReencuentraMascotas/',
+  
+    build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
